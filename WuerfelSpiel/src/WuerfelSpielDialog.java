99ac4df981e698f6c83aca6777fd5c0d2c2aca8e
jak2011math <asdasd007>
2012-04-22 20:19:47 +0200

    asd

--- WuerfelSpiel/src/WuerfelSpielDialog.java
+++ WuerfelSpiel/src/WuerfelSpielDialog.java
@@ -4,5 +4,36 @@ public class WuerfelSpielDialog {
 	public String [] Spieler = new String[anzahl];
 	
 
+System.out.println( "Mehrere Spieler würfeln hintereinander. Sieger ist der Spieler mit der höchsten Augensumme. 1. Variante: Alle Augen werden addiert. 2. Variante: Nur die geraden Augen werden addiert. 3. Variante: Alle Augen werden addiert, würfelt man eine Eins, wird die bis dahin erreichte Augensumme wieder gelöscht.");
+	
+	
+	
+private int anzahl = IOTools.readInteger
+				( "Bitte die gewünschte Spieleranzahl eingeben!");
+private int rundenanzahl = IOTools.readInteger
+				( "Bitte die gewünschte Rundenanzahl eingeben!");
+private int variante = IOTools.readInteger
+ 				( "Bitte die gewünschte Variante 1,2 oder 3 eingeben eingeben!");
+
+
+if (variante != 1 || variante != 2 || variante != 3)
+		{
+		int variante = IOTools.readInteger
+				( "Bitte die gewünschte Variante 1,2 oder 3 eingeben eingeben!");
+		}
+
+
+public int getAnzahl() {
+	return anzahl;
+	}
+
+public int getRundenanzahl() {
+	return rundenanzahl;
+	}
+
+public int getVariante() {
+	return variante;
+	}
+
 
 }
