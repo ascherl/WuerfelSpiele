1cbc41b94a3eff58cf9bbc059a990aa6558333af
Rudi Ascherl <info@rudiascherl.de>
2012-04-22 22:01:34 +0200

    Test-Commit

--- WuerfelSpiel/src/TestSpiel.java
+++ WuerfelSpiel/src/TestSpiel.java
@@ -1,4 +1,7 @@
 
+import Tools.IO.*;
+
+
 public class TestSpiel {
 
 	/**
@@ -6,7 +9,12 @@ public class TestSpiel {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String[] spieler = { "Heinz", "Kai-Uwe", "Paul-Werner"};
+		
+		Wuerfel wuerfel = new Wuerfel(1,6);
+		
+		int zahl = IOTools.readInteger("Was für eine Zahl? ")
+		System.out
 	}
 
 }
---
+++ WuerfelSpiel/src/WuerfelSpiele.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * @author rudi
+ * 
+ */
+public class WuerfelSpiele {
+	
+	public class 
+	
+	public class neuesSpiel {
+		// create new player for each input
+		for(int i=0; i <= spieler.length ; i++) {
+			Spieler Spieler_i = new Spieler(); 
+		}
+	}
+	
+	public class wuerfeln {
+		
+		class wuefeln(Spieler name, int dran) {
+			Wuerfel wuerfel = new Wuerfel();
+			Spieler[dran].augen[runde] = wuerfel.getAugen();
+		}
+	}
+	
+	public class auswertung {
+		
+	}
+}
