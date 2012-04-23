2e49352a3126dd7dbaedd89f5aa49d67907e16b4
jak2011math <asdasd007>
2012-04-23 21:50:52 +0200

    asd

--- WuerfelSpiel/src/Spieler.java
+++ WuerfelSpiel/src/Spieler.java
@@ -1,36 +1,55 @@
-
+// Spieler.java                                 JG 2012
 
 import Tools.IO.*;                          // Eingaben
 
-public class Spieler {
-	
-	public static final int rundenzahl = WuerfelSpiele.getRundenanzahl();
-	private String name;
-	private int [] augen = new int[rundenzahl];
-	
-	
+/**
+ * Spieler fuer ein Würfelspiel.
+ */
+public class Spieler
+{
+/* ------------------------------------------------- */
+                                           // Attribute
 
+
+/**
+ * Name des Spielers.
+ */
+  String name = "";
+
+
+
+/* ------------------------------------------------- */
+                                            // Programm
+/**
+ * Test der Klasse Spieler
+ */
+  public static void addSpieler()
+  {
+                          // Einlesen der Spieleranzahl
+    int spielerAnzahl
+    = WuerfelSpielDialog.setAnzahl();
+
+                  // Erzeugen eines Feldes von Spielern
+    Spieler[] spieler = new Spieler[ WuerfelSpielDialog.setAnzahl()];
+
+  // Bereitstellen des Datenspeichers für jeden Spieler
+    for( int i = 0; i < spieler.length; i++)
+      spieler[ i] = new Spieler();
+
+                           // Einlesen der Spielerdaten
+    for( int i = 0; i < spieler.length; i++)
+    {
+      System.out.println( "Spieler "+ (i + 1));
+      spieler[ i].name
+      = IOTools.readLine( "Wie ist Dein Vorname? ");
+     
+    }
+
+           // Auflisten aller eingelesenen Spielerdaten
+    System.out.println( "Es spielen mit: ");
+    for( int i = 0; i < spieler.length; i++)
+      System.out.println
+      ( "Spieler "+ (i + 1) + " " + 
+      spieler[ i].name);
+  }
 }
-	
-
-	
-				Spieler(String name){
-						this.name = name;
-		
-						}
-	
-				public String getName() {
-					return name;
-					}
-
-				public void setName(String name) {
-					this.name = name;
-					}
-				
-				
-				public void addSpieler()
-				{
-					this.addSpieler(getSpieler);
-				}
-				
-				
