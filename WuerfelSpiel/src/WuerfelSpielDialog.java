e254da5f815ea090fc94332dba2964b0b0b4df0b
Rudi Ascherl <info@rudiascherl.de>
2012-04-24 11:49:02 +0200

    Comments for Dialogue

--- WuerfelSpiel/src/WuerfelSpielDialog.java
+++ WuerfelSpiel/src/WuerfelSpielDialog.java
@@ -6,42 +6,72 @@ public static int rundenanzahl;
 private static int anzahl;
 private static int variante;
 
-
+/**
+ * Fragt die Spieleranzahl vom Benutzer ab.
+ * @return
+ */
 public static int setAnzahl() {
 	int anzahl = IOTools.readInteger( "Bitte die gewuenschte Spieleranzahl eingeben!  ");
 	return anzahl;
 	}
 
+/**
+ * Fragt die Rundenanzahl vom Benutzer ab.
+ * @return
+ */
 public static int setRundenanzahl() {
 	int rundenanzahl = IOTools.readInteger
 			( "Bitte die gewuenschte Rundenanzahl eingeben!  ");
 	return rundenanzahl;
 	}
 
+/**
+ * Fragt die Variante vom Benutzer ab.
+ * @return
+ */
 public static int setVariante() {
 	int variante = IOTools.readInteger
 				( "Bitte die gewuenschte Variante 1,2 oder 3 eingeben eingeben!  ");
 	return variante;
 }
 
+/**
+ * Gibt die Rundenanzahl aus.
+ * @return Rundenanzahl
+ */
 public static int getRundenanzahl() {
 	return rundenanzahl;
 
 }
 
+/**
+ * Gibt die Spieleranzahl aus.
+ * @return Spieleranzahl
+ */
 public static int getAnzahl() {
 	return anzahl;
 	}
 
+/**
+ * Gibt die gewŠhlte Variante aus.
+ * @return Variante
+ */
 public static int getVariante() {
 	return variante;
 }
 
+/**
+ * Ausgabe von Spieler.auswertung
+ * @param name Spielername
+ * @param ergebnis Gesamtpunktzahl nach Auswertung
+ */
 public static void Auswertung(String name, int ergebnis){
 	System.out.println(name + ", Du hast aktuell " + ergebnis + " Punkte!");
 }
 
-
+/**
+ * Gibt die Spielregeln aus.
+ */
 public static void getSpielregeln() {
 	System.out.println( "Mehrere Spieler wuerfeln hintereinander.");
 	System.out.println( "Sieger ist der Spieler mit der hoechsten Augensumme.");	
